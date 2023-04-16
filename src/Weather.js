a3cef45d682ae02eeaff6b000023df4bdb0d6cd3
mayflower865 <mayflowerlongfellow@gmail.com>
2023-04-16 14:31:21 -0600

    Added axios intergration so it shows the weather of a city

---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import React from "react";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = "5293d8454b519c30f6f6331f38c85b4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return 
+}
