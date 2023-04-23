080cd44be9c8d68d55f2b9823dc9826aedcc069d
mayflower865 <mayflowerlongfellow@gmail.com>
2023-04-23 15:43:44 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>Weather App</h1>
       <footer>
         Coded with 💗 by{" "}
@@ -12,6 +15,7 @@ export default function App() {
            May Longfellow
         </a>
       </footer>
+      </div>
     </div>
   );
 }
