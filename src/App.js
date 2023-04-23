fff8e4d6cfc340e9e9a25eb3d2b7ec3f960f5660
mayflower865 <mayflowerlongfellow@gmail.com>
2023-04-23 13:03:35 -0600

    creating a open sourced github link and h1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
-import Weather from "./Weather";
 import React from "react";
 
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+     <h1>Weather App</h1>
+      <footer>
+        Coded with 💗 by{" "}
+        <a href="https://github.com/mayflower865/weather.react" target="_blank">
+           May Longfellow
+        </a>
+      </footer>
+    </div>
+  );
 }
 
