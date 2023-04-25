0c271180af54f6699189d84232752978e2a1fe93
mayflower865 <mayflowerlongfellow@gmail.com>
2023-04-25 10:29:11 -0600

    adding CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
-
+import Weather from "./Weather";
 
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-     <h1>Weather App</h1>
+     <Weather />
       <footer>
         Coded with 💗 by{" "}
         <a href="https://github.com/mayflower865/weather.react" target="_blank">
--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,42 @@
-import axios from "axios";
+import React from "react";
+import "./Weather.css";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+export default function Weather() {
+    return (
+        <div className="weather">
+            <form>
+                <div className="row">
+                <div className="col-9">
+                <input type="search" placeholder="Enter your city..." className="form-control" autoFocus="on" />
+                </div>
+                <div className="col-3">
+                <input type="submit" value="search" className="btn btn-primary w-100" />
+                </div>
+                </div>
+            </form>
+            <h1>Clarksville</h1>
+            <ul>
+                <li>Wednesday 07:00</li>
+                <li>Mostly Cloudy</li>
+            </ul>
+            <div className="row mt-3">
+                <div className="col-6">
+                    <div className="clearfix">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly cloudy" className="float-left" />
+                <div className="float-left">
+                <span className="temperature">6</span>
+                <span className="unit">°C</span>
+                </div>
+                </div>
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>precipitation: 15%</li>
+                        <li>Humidity: 72%</li>
+                        <li>Wind: 13 km/h</li>
+                    </ul>
+                </div>
+            </div>
+            </div>
     );
-  }
-  let apiKey = "5293d8454b519c30f6f6331f38c85b4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return 
 }
